ce47655a104a0fcae6ddb125565e474a6cc9bcfd
Carlos Hernandez <carlosllaocua@gmail.com>
2023-10-31 20:36:06 -0500

    Componente Antecedente

---
+++ src/app/components/antecedente/crear-antecedente/crear-antecedente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-antecedente',
+  templateUrl: './crear-antecedente.component.html',
+  styleUrls: ['./crear-antecedente.component.css']
+})
+export class CrearAntecedenteComponent {
+
+}
