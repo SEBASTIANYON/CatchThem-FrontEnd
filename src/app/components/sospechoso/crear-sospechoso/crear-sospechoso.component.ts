5034afe96ec6cf02f8e076a3151b2ae3d435d37f
Carlos Hernandez <carlosllaocua@gmail.com>
2023-10-31 20:36:23 -0500

    Componente Sospechoso

--- src/app/components/sospechoso/crear-sospechoso/crear-sospechoso.component.ts
+++ src/app/components/sospechoso/crear-sospechoso/crear-sospechoso.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  FormControl,
+} from '@angular/forms';
+import { ActivatedRoute, Params, Route, Router } from '@angular/router';
+import { Sospechoso } from 'src/app/models/Sospechoso';
+import { SospechosoService } from 'src/app/services/sospechoso.service';
+
 
 @Component({
   selector: 'app-crear-sospechoso',
@@ -7,4 +18,60 @@ import { Component } from '@angular/core';
 })
 export class CrearSospechosoComponent {
 
+
+  form: FormGroup = new FormGroup({});
+  sospechoso: Sospechoso = new Sospechoso();
+  mensaje: string = '';
+
+  tipos: { value: string; viewValue: string }[] = [
+    { value: 'Femenino', viewValue: 'Femenino' },
+    { value: 'Masculino', viewValue: 'Masculino' }
+  ];
+
+  constructor(
+    private sS: SospechosoService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      idSospechoso: [''],
+      nombre: ['', Validators.required],
+      alias: ['', Validators.required],
+      nacimiento: ['', Validators.required],
+      genero:["",Validators.required],
+      nacionalidad:["",Validators.required],
+      descripcion:[""],
+      estado:["",Validators.required],
+
+    });
+  }
+
+  aceptar(): void {
+    if (this.form.valid) {
+      this.sospechoso.idSospechoso = this.form.value.idSospechoso;
+      this.sospechoso.nombre = this.form.value.nombre;
+      this.sospechoso.alias = this.form.value.alias;
+      this.sospechoso.nacimiento = this.form.value.nacimiento;
+      this.sospechoso.nacionalidad=this.form.value.nacionalidad;
+
+      this.sS.insert(this.sospechoso).subscribe((data) => {
+        this.sS.list().subscribe((data) => {
+          this.sS.setList(data);
+        });
+      });
+
+      this.router.navigate(['sospechosos']);
+    }
+  }
+
+  obtenerControlCampo(nombreCampo: string): AbstractControl {
+    const control = this.form.get(nombreCampo);
+    if (!control) {
+      throw new Error(`Control no encontrado para el campo ${nombreCampo}`);
+    }
+    return control;
+  }
 }
--- src/app/components/sospechoso/listar-sospechoso/listar-sospechoso.component.ts
+++ src/app/components/sospechoso/listar-sospechoso/listar-sospechoso.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SospechosoService } from 'src/app/services/sospechoso.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Sospechoso } from 'src/app/models/Sospechoso';
 
 @Component({
   selector: 'app-listar-sospechoso',
   templateUrl: './listar-sospechoso.component.html',
-  styleUrls: ['./listar-sospechoso.component.css']
+  styleUrls: ['./listar-sospechoso.component.css'],
 })
-export class ListarSospechosoComponent {
+export class ListarSospechosoComponent implements OnInit {
+  dataSource: MatTableDataSource<Sospechoso> = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  displayedColumns: string[] = [
+    'idSospechoso',
+    'nombre',
+    'alias',
+    'nacimiento',
+    'genero',
+    'nacionalidad'
+  ];
+  constructor(private uS: SospechosoService) {}
+  ngOnInit(): void {
+    this.uS.list().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.uS.getList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
 
+  //se agrega eliminar por id
+  eliminar(id: number) {
+    this.uS.delete(id).subscribe((data) => {
+      this.uS.list().subscribe((data) => {
+        this.uS.setList(data);
+      });
+    });
+  }
+
+  filter(en: any) {
+    this.dataSource.filter = en.target.value.trim();
+  }
 }
