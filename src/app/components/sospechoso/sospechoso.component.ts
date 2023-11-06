71724bae17e7d2a4e600b15b0da3f02a1e986c68
Joaquín <joaquin20040107@gmail.com>
2023-11-06 01:00:07 -0500

    Avance de Sospechoso

--- src/app/components/sospechoso/sospechoso.component.ts
+++ src/app/components/sospechoso/sospechoso.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sospechoso',
   templateUrl: './sospechoso.component.html',
-  styleUrls: ['./sospechoso.component.css']
+  styleUrls: ['./sospechoso.component.css'],
 })
 export class SospechosoComponent {
-
+  constructor(public route: ActivatedRoute) {}
 }
