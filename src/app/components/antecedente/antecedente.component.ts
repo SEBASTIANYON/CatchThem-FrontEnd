e9a2aaea750ee850553dda28590ee8ee7e3c3b0e
Carlos Hernandez <carlosllaocua@gmail.com>
2023-11-07 18:38:44 -0500

    Componente Antecedente

--- src/app/components/antecedente/antecedente.component.ts
+++ src/app/components/antecedente/antecedente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-antecedente',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./antecedente.component.css']
 })
 export class AntecedenteComponent {
+
+  constructor(public route:ActivatedRoute){}
 
 }
