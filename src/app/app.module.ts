d9560ec517b8f8f73faf2776819820ccdc84df9e
Joaquín <joaquin20040107@gmail.com>
2023-11-08 18:54:37 -0500

    login con seguridad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,69 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EntidadComponent } from './components/entidad/entidad.component';
-import { CrearEntidadComponent } from './components/entidad/crear-entidad/crear-entidad.component';
-import { ListarEntidadComponent } from './components/entidad/listar-entidad/listar-entidad.component';
-import { SospechosoComponent } from './components/sospechoso/sospechoso.component';
-import { CrearSospechosoComponent } from './components/sospechoso/crear-sospechoso/crear-sospechoso.component';
-import { ListarSospechosoComponent } from './components/sospechoso/listar-sospechoso/listar-sospechoso.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { NgFor } from '@angular/common';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { CamaraComponent } from './components/camara/camara.component';
-import { ListarCamaraComponent } from './components/camara/listar-camara/listar-camara.component';
-import { CrearCamaraComponent } from './components/camara/crear-camara/crear-camara.component';
-import { MatCardModule } from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LoginComponent } from './components/login/login.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EntidadComponent,
-    CrearEntidadComponent,
-    ListarEntidadComponent,
-    SospechosoComponent,
-    CrearSospechosoComponent,
-    ListarSospechosoComponent,
-    CamaraComponent,
-    ListarCamaraComponent,
-    CrearCamaraComponent,
-  ],
+    LoginComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     HttpClientModule,
-    MatIconModule,
+    BrowserAnimationsModule,
     MatButtonModule,
-    MatToolbarModule,
-    MatTabsModule,
-    NgFor,
-    MatSelectModule,
     MatInputModule,
-    MatFormFieldModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatMenuModule,
     FormsModule,
     ReactiveFormsModule,
-    MatMenuModule,
-    MatPaginatorModule,
-    MatTableModule,
-    MatCardModule,
-    MatGridListModule,
-    MatNativeDateModule,
-    MatDatepickerModule
+    MatSnackBarModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
