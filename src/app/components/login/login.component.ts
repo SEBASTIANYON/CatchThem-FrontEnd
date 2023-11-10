962373beb248b2eac59463508d81238b487cf8c3
Sebastian Enrique Yon Alegre <u202113053@upc.edu.pe>
2023-11-09 19:56:46 -0500

    cambio de pantalla temporal luego de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
     request.password = this.password;
     this.loginService.login(request).subscribe((data: any) => {
       sessionStorage.setItem("token", data.jwttoken);
-      this.router.navigate(['components/entidades']);
+      this.router.navigate(['/sospechosos']);
     }, error => {
       this.mensaje = "Credenciales incorrectas!!!"
       this.snackBar.open(this.mensaje, "Aviso",{duration:2000});
