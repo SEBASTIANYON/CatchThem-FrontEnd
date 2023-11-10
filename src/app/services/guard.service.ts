2b52efbaa0b69bfd049c4db7ba5b33323d63c7bb
Sebastian Enrique Yon Alegre <u202113053@upc.edu.pe>
2023-11-09 19:57:57 -0500

    configuracion de guard de rutas segun rol

--- src/app/services/guard.service.ts
+++ src/app/services/guard.service.ts
@@ -5,17 +5,28 @@ import { LoginService } from './login.service';
 @Injectable({
   providedIn: 'root'
 })
-export class GuardService{
+export class GuardService {
 
-  constructor(private lService:LoginService, private router:Router) {
+  constructor(private lService: LoginService, private router: Router) {
 
   }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const rpta=this.lService.verificar();
-    if(!rpta){
-      this.router.navigate(['/login']);
-      return false;
+
+    const rpta = this.lService.verificar();
+
+    if (rpta) {
+      const rol = this.lService.showRole()
+
+      if (route.routeConfig?.data?.['role'] === undefined || route.routeConfig?.data?.['role']?.indexOf(rol) > -1) {
+        return true
+      }
+
+      return false
     }
-    return rpta;
+
+    this.router.navigate(['/login']);
+    return false;
+
   }
+
 }
