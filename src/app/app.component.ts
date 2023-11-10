fc9e4ef777e3fd11c2e2002172008e6cc7b780fb
Sebastian Enrique Yon Alegre <u202113053@upc.edu.pe>
2023-11-09 19:54:17 -0500

    configuracion de token con datos de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,23 @@ import { LoginService } from './services/login.service';
 export class AppComponent {
   title = 'catchthem';
   role: string = '';
-
+  username: string = ''
+  entidad: string = ''
   constructor(private loginService: LoginService) {}
 
   cerrar() {
     sessionStorage.clear();
   }
 
+
   verificar() {
     this.role = this.loginService.showRole();
+    this.username = this.loginService.showUsername();
+    this.entidad = this.loginService.showEntidad();
     return this.loginService.verificar();
   }
   validarRol() {
-    if (this.role == 'ADMIN' || this.role == 'USER') {
+    if (this.role == 'ADMIN' || this.role == 'POLICIA' || this.role == 'AGENTE') {
       return true;
     } else {
       return false;
