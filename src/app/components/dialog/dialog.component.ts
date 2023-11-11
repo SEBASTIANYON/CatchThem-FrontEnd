855368f3dcb51653ae1016dee060808a7f7de9cb
Sebastian Enrique Yon Alegre <u202113053@upc.edu.pe>
2023-11-10 23:15:59 -0500

    crear dialogo de confirmacion y mapa

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,23 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.css']
 })
-export class DialogComponent implements OnInit{
-  constructor(public dialog: MatDialogRef<DialogComponent>){}
-
-  closeDialog(){
-    this.dialog.close(false)
-  }
-
-  confirmDialog(){
-    this.dialog.close(true)
-  }
-
-  ngOnInit(): void {
-    
-  }
-
+export class DialogComponent {
+  
 }
