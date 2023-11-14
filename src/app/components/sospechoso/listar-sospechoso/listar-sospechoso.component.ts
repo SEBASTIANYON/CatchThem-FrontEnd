9e3aa30f70961336ac20fb93d3b963075b91229f
Joaquín <joaquin20040107@gmail.com>
2023-11-14 02:50:58 -0500

    funcion de eliminar

--- src/app/components/sospechoso/listar-sospechoso/listar-sospechoso.component.ts
+++ src/app/components/sospechoso/listar-sospechoso/listar-sospechoso.component.ts
@@ -1,6 +1,8 @@
 import { SospechosoService } from './../../../services/sospechoso.service';
 import { Sospechoso } from 'src/app/models/Sospechoso';
 import { Component, OnInit } from '@angular/core';
+import { DialogoConfirmacionComponent } from '../../dialog/dialogo-confirmacion/dialogo-confirmacion.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-listar-sospechoso',
@@ -9,8 +11,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarSospechosoComponent implements OnInit {
   public sospechoso: Sospechoso[] = [];
+  
 
-  constructor(private oS: SospechosoService) {}
+  constructor(private oS: SospechosoService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.oS.list().subscribe((data: Sospechoso[]) => {
@@ -27,5 +30,15 @@ export class ListarSospechosoComponent implements OnInit {
         this.oS.setList(data);
       });
     });
+  }
+
+  openDialog(idSospechoso: number){
+    this.dialog.open(DialogoConfirmacionComponent)
+    .afterClosed()
+    .subscribe((confirmacion: Boolean) => {
+      if(confirmacion){
+        this.eliminar(idSospechoso)
+      }
+    })
   }
 }
