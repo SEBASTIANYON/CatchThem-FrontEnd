f40fbcf376b8f0b172d904c8f1d474c823334904
MoralesSil <102762790+MoralesSil@users.noreply.github.com>
2023-11-16 10:04:47 -0500

    tipo

--- src/app/components/tipo-entidad/listar-tipo-entidad/listar-tipo-entidad.component.ts
+++ src/app/components/tipo-entidad/listar-tipo-entidad/listar-tipo-entidad.component.ts
@@ -4,6 +4,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { TipoEntidadService } from 'src/app/services/tipoentidad.service';
+import { DialogoConfirmacionComponent } from '../../dialog/dialogo-confirmacion/dialogo-confirmacion.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-listar-tipo-entidad',
@@ -16,9 +18,13 @@ export class ListarTipoEntidadComponent implements OnInit {
   displayedColumns: string[] = [
     'idTipo',
     'sector',
+    'actualizar',
+    'eliminar'
   ];
   role: string = ''
-  constructor(private iS: TipoEntidadService, private loginService:LoginService) {}
+  constructor(private iS: TipoEntidadService, private loginService:LoginService,
+    public dialog: MatDialog) {}
+
   ngOnInit(): void {
     this.iS.list().subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
@@ -29,8 +35,25 @@ export class ListarTipoEntidadComponent implements OnInit {
       this.dataSource.paginator = this.paginator;
     });
     this.role = this.loginService.showRole()
+
+    if(this.role !== 'AGENTE'){
+      this.displayedColumns = [
+        'idTipo',
+        'sector',
+      ];
+    }
   }
 
+  openDialog(idTipo: number){
+    this.dialog.open(DialogoConfirmacionComponent)
+    .afterClosed()
+    .subscribe((confirmacion: Boolean) => {
+      if(confirmacion){
+        this.eliminar(idTipo)
+      }
+    })
+  }
+  
   eliminar(id: number) {
     this.iS.delete(id).subscribe((data) => {
       this.iS.list().subscribe((data) => {
@@ -42,4 +65,5 @@ export class ListarTipoEntidadComponent implements OnInit {
   filter(en:any){
     this.dataSource.filter=en.target.value.trim();
   }
+
 }
