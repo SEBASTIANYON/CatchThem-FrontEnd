c081ccecf28e834a3f6aeee1aecdd1b5de98df6f
MoralesSil <102762790+MoralesSil@users.noreply.github.com>
2023-11-18 19:06:43 -0500

    tipo

--- src/app/services/tipoentidad.service.ts
+++ src/app/services/tipoentidad.service.ts
@@ -21,7 +21,12 @@ export class TipoEntidadService {
   }
 
   insert(te: TipoEntidad) {
-    return this.http.post(this.url, te);
+    let token = sessionStorage.getItem('token');
+    return this.http.post(this.url, te, {
+    headers: new HttpHeaders()
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json'),
+    });
   }
 
   setList(listaNueva: TipoEntidad[]) {
@@ -33,14 +38,29 @@ export class TipoEntidadService {
   }
 
   listId(id: number) {
-    return this.http.get<TipoEntidad>(`${this.url}/${id}`);
+    let token = sessionStorage.getItem('token');
+    return this.http.get<TipoEntidad>(`${this.url}/${id}`, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
 
   update(te: TipoEntidad) {
-    return this.http.put(this.url, te);
+    let token = sessionStorage.getItem('token');
+    return this.http.put(this.url, te, {
+      headers: new HttpHeaders()
+        .set('Authorization', `Bearer ${token}`)
+        .set('Content-Type', 'application/json'),
+    });
   }
-
+  
   delete(id: number) {
-    return this.http.delete(`${this.url}/${id}`);
+    let token = sessionStorage.getItem('token');
+    return this.http.delete(`${this.url}/${id}`, {
+    headers: new HttpHeaders()
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json'),
+    });
   }
 }
